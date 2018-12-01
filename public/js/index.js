9eca5074fafd3892d5c5710db1c8056f4593085e
jignesh-russmediatech <jignesh.dadhaniya@russmediatech.in>
2018-12-01 19:23:27 +0530

    jquery with text box and form

--- public/js/index.js
+++ public/js/index.js
@@ -13,6 +13,21 @@ socket.on('disconnect', function () {
   console.log('Discounnected from server');
 });
 
-socket.on('newMessage', function (data) {
-  console.log('newMessage', data);
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  var li = $('<li></li>');
+  li.text(`${message.from}: ${message.txt}`);
+
+  $('#messages').append(li);
+});
+
+$('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    txt: $('[name=message]').val()
+  }, function () {
+    //console.log('Got it', data);
+  });
 });
