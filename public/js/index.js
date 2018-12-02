7821188ca371af289ac4277972ebdea690d8c7ae
jignesh-russmediatech <jignesh.dadhaniya@russmediatech.in>
2018-12-02 17:14:41 +0530

    scroll added

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,19 @@
 var socket = io();
+function scrollToBottom() {
+  var messages = $('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop    = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    console.log('Scroll top');
+    messages.scrollTop(scrollHeight);
+  }
+}
 
 socket.on('connect', function () {
   console.log('Connected to server');
@@ -28,6 +43,8 @@ socket.on('newMessage', function (message) {
   });
 
   $('#messages').append(html);
+
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -47,6 +64,8 @@ socket.on('newLocationMessage', function(message) {
   });
 
   $('#messages').append(html);
+
+  scrollToBottom();
 });
 
 $('#message-form').on('submit', function (e) {
